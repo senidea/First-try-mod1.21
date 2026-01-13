d95fad2e471026db4c8c2b936d3e43cae14bd515
senidea <chep.ymyr@gmail.com>
2026-01-13 17:52:47 +0100

    Commit

--- src/main/java/net/senidea/firsttry/FirstTry.java
+++ src/main/java/net/senidea/firsttry/FirstTry.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//haha
 public class FirstTry implements ModInitializer {
 	public static final String MOD_ID = "firsttry";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
