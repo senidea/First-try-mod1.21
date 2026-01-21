8f1872899879db43d8ce4c21db0717987f4c4a88
senidea <chep.ymyr@gmail.com>
2026-01-21 18:09:21 +0100

    Raw Pink Garnet

--- src/main/java/net/senidea/firsttry/item/ModItems.java
+++ src/main/java/net/senidea/firsttry/item/ModItems.java
@@ -14,7 +14,7 @@ public class ModItems {
 
 
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
-
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
 
     private static Item registerItem(String  name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(FirstTry.MOD_ID, name), item);
@@ -24,6 +24,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries ->{
             fabricItemGroupEntries.add(PINK_GARNET);
+            fabricItemGroupEntries.add(RAW_PINK_GARNET);
         });
     }
 }
