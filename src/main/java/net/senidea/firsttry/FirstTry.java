939f5ed197bb8d7712a9f7da90fc27aa784a0239
senidea <chep.ymyr@gmail.com>
2026-01-28 17:53:44 +0100

    Added pink garnet block(almost)

--- src/main/java/net/senidea/firsttry/FirstTry.java
+++ src/main/java/net/senidea/firsttry/FirstTry.java
@@ -2,6 +2,7 @@ package net.senidea.firsttry;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.senidea.firsttry.block.ModBlocks;
 import net.senidea.firsttry.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class FirstTry implements ModInitializer {
 	@Override
 	public void onInitialize() {
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
 	}
 }
